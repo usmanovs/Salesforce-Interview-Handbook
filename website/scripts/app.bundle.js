1df13aad7c6fc7557a258e409d077eaf5459af92
creativegeniusod <creativegenius2909@gmail.com>
2019-11-15 12:18:12 +0530

    right sidebar fixed

--- website/scripts/app.bundle.js
+++ website/scripts/app.bundle.js
@@ -517,7 +517,7 @@
 				  var windowTop = $(window).scrollTop(); // returns number
 
 				  if (stopPoint < windowTop) {
-					  $sticky.css({ position: 'absolute', top: diff });
+					  $sticky.css({ position: 'absolute', top: '15px' });
 				  } else if (stickyTop < windowTop+stickOffset) {
 					  $sticky.css({ position: 'fixed', top: '15px' });
 				  } else {
